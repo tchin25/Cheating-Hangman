e60a757f7d9d1d2c4b73ce2654161e95942580e1
Thomas Chin <tchin25@gmail.com>
2018-03-26 13:39:49 -0400

    Beginning Set Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
 
+	static Set<String> dict = new HashSet<String>(); 
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Thomas");
 
 	}
-
+	
+	//Import Dictionary into set
+	public static void importWords() throws FileNotFoundException {
+		Scanner in = new Scanner(new File("Dictionary.txt"));
+		
+		while (in.hasNextLine()) {
+			dict.add(in.nextLine());
+		}
+	}
 }
