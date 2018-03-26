b5b9e2591027dc468bbf6d462c5ffc6c0c1f40bb
Thomas Chin <tchin25@gmail.com>
2018-03-26 13:59:34 -0400

    Word Set

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,25 @@ import java.util.*;
 
 public class Main {
 
-	static Map<String, Integer> dict = new HashMap<String, Integer>(); 
+	static Set<Word> dict = new HashSet<Word>(); 
 	
 	public static void main(String[] args) {
 		importWords();
+		
+		for(Word word : dict) {
+			System.out.println(word.s + " : " + word.length);
+		}
 
 	}
 	
-	//Import Dictionary into set
+	//Import Dictionary into map
 	public static void importWords() {
 		Scanner in;
 		try {
 			in = new Scanner(new File("Dictionary.txt"));
 			while (in.hasNextLine()) {
 				String s = in.nextLine();
-				dict.put(s, s.length());
+				dict.add(new Word(s));
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found");
---
+++ src/Word.java
@@ -0,0 +1,10 @@
+	public class Word{
+		
+		String s;
+		int length;
+		
+		public Word(String s) {
+			this.s = s;
+			length = s.length();
+		}
+	}
