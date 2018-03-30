341cc2330ed24f5d98011d9a1f4a11dbe75f075f
Nicolas dos Santos <nicolas182@icloud.com>
2018-03-29 23:56:32 -0400

    Cleaned up a little

--- src/Main.java
+++ src/Main.java
@@ -5,157 +5,156 @@ import java.util.*;
 public class Main {
 
 
-	static Set<Word> dict = new HashSet<Word>();
-
-	public static void main(String[] args) {
-		if (!importWords())
-			return;
-
-		Set<Word> x = userInput();
-
-		for (Word w : x) {
-			System.out.println(w);
-		}
-		System.out.println("---------------------");
+    static Set<Word> dict = new HashSet<Word>();
 
 
-		Set<Word> y = sortFamily(x, 'a');
+    public static void main(String[] args) {
+        menu();
+    }
 
-		for (Word w : y) {
-			System.out.println(w);
-		}
-		sortNumChar(y, 'a');
 
-	}
+    //Import Dictionary into set
+    public static boolean importWords() {
+        try {
+            Scanner in = new Scanner(new File("test.txt"));
+            while (in.hasNextLine()) {
+                String s = in.nextLine();
+                dict.add(new Word(s));
+            }
+            in.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            //e.printStackTrace();
+            return false;
+        }
+    }
 
-	//Import Dictionary into set
-	public static boolean importWords() {
-		try {
-			Scanner in = new Scanner(new File("test.txt"));
-			while (in.hasNextLine()) {
-				String s = in.nextLine();
-				dict.add(new Word(s));
-			}
-			in.close();
-			return true;
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-			//e.printStackTrace();
-			return false;
-		}
-	}
 
-	public static Set<Word> sortFamily(Set<Word> w, char guess) {
-		Map<Integer, Set<Word>> families = new HashMap<Integer, Set<Word>>();
-		for (Word word : w) {
+    public static Set<Word> sortFamily(Set<Word> w, char guess) {
+        Map<Integer, Set<Word>> families = new HashMap<Integer, Set<Word>>();
+        for (Word word : w) {
 
-			int counter = 0; // counter for number of char in word
-			for (int i = 0; i < word.length; i++) {
-				if (word.s.charAt(i) == guess) {
-					counter++;
-				}
-			}
+            int counter = 0; // counter for number of char in word
+            for (int i = 0; i < word.length; i++) {
+                if (word.s.charAt(i) == guess) {
+                    counter++;
+                }
+            }
 
-			//inserts words into family groups
-			if (!families.containsKey(counter)) {
-				families.put(counter, new HashSet<Word>());
-			}
-			families.get(counter).add(word);
-		}
+            //inserts words into family groups
+            if (!families.containsKey(counter)) {
+                families.put(counter, new HashSet<Word>());
+            }
+            families.get(counter).add(word);
+        }
 
-		//outputs largest set in families
-		int size = 0;
-		int index = 0;
-		for (Map.Entry<Integer, Set<Word>> k : families.entrySet()) {
-			if (k.getValue().size() > size) {
-				index = k.getKey();
-				size = k.getValue().size();
-			}
-		}
+        //outputs largest set in families
+        int size = 0;
+        int index = 0;
+        for (Map.Entry<Integer, Set<Word>> k : families.entrySet()) {
+            if (k.getValue().size() > size) {
+                index = k.getKey();
+                size = k.getValue().size();
+            }
+        }
 
-		//System.out.println(families.entrySet());
+        //System.out.println(families.entrySet());
 
-		//returns largest set
-		return families.get(index);
-	}
+        //returns largest set
+        return families.get(index);
+    }
 
-	/*****************
-	 TODO: Create method that takes a set and guessed letter and sorts it by location of guessed letter within each word
-	 *	  Returns a set with the largest selection of words to cheat on
-	 *	  DOESN"T DO ANYTHING IF THE GUESSED LETTER ISN'T WITHIN THE SET OF WORDS
-	 *	  This method and sortFamily should feed into each other with guesses until guesses loses or there's 2 words left,
-	 *	  which is then handled in another method so that the guesser always guesses incorrectly
-	 *****************/
-	public static Set<Word> sortNumChar(Set<Word> w, char guess) {
-		//test how many times char shows up
-		Map<ArrayList<Integer>, Set<Word>> letterPosition = new HashMap<ArrayList<Integer>, Set<Word>>();
 
-		for (Word word : w) {
-			ArrayList<Integer> positions = new ArrayList<Integer>();
+    public static Set<Word> sortNumChar(Set<Word> w, char guess) {
+        //test how many times char shows up
+        Map<ArrayList<Integer>, Set<Word>> letterPosition = new HashMap<ArrayList<Integer>, Set<Word>>();
 
-			char letters[] = word.s.toCharArray();
-			for (int i = 0; i < word.length; i++) {
-				//System.out.println(letters[i]);
-				if (letters[i] == guess)
-				{
-					//System.out.println(letters[i]);
-					positions.add(i);
-				}
+        for (Word word : w) {
+            ArrayList<Integer> positions = new ArrayList<Integer>();
 
-			}
+            char letters[] = word.s.toCharArray();
+            for (int i = 0; i < word.length; i++) {
+                //System.out.println(letters[i]);
+                if (letters[i] == guess) {
+                    //System.out.println(letters[i]);
+                    positions.add(i);
+                }
 
-            if (positions.isEmpty()){
+            }
+
+            if (positions.isEmpty()) {
                 return w;
 
-            }else{
-                if(!letterPosition.containsKey(positions)) {
+            } else {
+                if (!letterPosition.containsKey(positions)) {
                     letterPosition.put(positions, new HashSet<Word>());
                 }
                 letterPosition.get(positions).add(word);
-//                for (Map.Entry<ArrayList<Integer>, Set<Word>> k : letterPosition.entrySet()) {
-//                    if (k.getKey().equals(positions)){
-//                        letterPosition.get(k.getKey()).add(word);
-//                    }
-//                }
+
             }
-            
+
 
             //System.out.println(positions);
 
-		}
+        }
         System.out.println(letterPosition.values());
         ArrayList<Integer> key = new ArrayList<Integer>();
         int size = 0;
         for (Map.Entry<ArrayList<Integer>, Set<Word>> k : letterPosition.entrySet()) {
-        	if (k.getValue().size() > size) {
-        		size = k.getValue().size();
-        		key = k.getKey();
-        	}
-        	
+            if (k.getValue().size() > size) {
+                size = k.getValue().size();
+                key = k.getKey();
+            }
+
         }
-		return letterPosition.get(key);
-	}
+        return letterPosition.get(key);
+    }
 
-		public static Set<Word> userInput () {
-			Set<Word> setOfWords = new HashSet<Word>();
+    //Prompts user for word length and creates and returns a set with all words of that length
+    public static Set<Word> userInput() {
+        Set<Word> setOfWords = new HashSet<Word>();
 
-			int wordLength;
+        int wordLength;
 
-			System.out.println("Please enter the word length:");
-			Scanner kb = new Scanner(System.in);
+        System.out.println("Please enter the word length:");
+        Scanner kb = new Scanner(System.in);
 
-			wordLength = kb.nextInt();
+        wordLength = kb.nextInt();
 
-			for (Word word : dict) {
-				if (word.length == wordLength) {
-					setOfWords.add(word);
+        for (Word word : dict) {
+            if (word.length == wordLength) {
+                setOfWords.add(word);
 
-					//System.out.println(word.s + " : " + word.length);
-				}
-			}
-			return setOfWords;
+                //System.out.println(word.s + " : " + word.length);
+            }
+        }
+        return setOfWords;
 
 
-		}
+    }
 
-	}
+    public static void menu() {
+
+
+        if (!importWords())
+            return;
+
+        Set<Word> x = userInput();
+
+        for (Word w : x) {
+            System.out.println(w);
+        }
+        System.out.println("---------------------");
+
+
+        Set<Word> y = sortFamily(x, 'a');
+
+        for (Word w : y) {
+            System.out.println(w);
+        }
+        sortNumChar(y, 'a');
+
+    }
+
+}
